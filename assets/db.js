c1b99763f88fe2183765af38e614f1a1f8b7cf81
agiskorska <skorskaga@gmail.com>
2022-07-25 15:16:45 +0100

    Add logic behind the comments display

---
+++ assets/db.js
@@ -0,0 +1,39 @@
+const comments = [
+    {
+        post_id: 0,
+        comments: [
+            {
+                content: 'I like it',
+                reactions: {
+                    heart: 50,
+                    like: 20,
+                    dislike: 1,
+                },
+            },
+            {
+                content: 'I love it',
+                reactions: {
+                    heart: 30,
+                    like: 18,
+                    dislike: 10,
+                },
+            },
+            {
+                content: 'I dont like it , op sucks!',
+                reactions: {
+                    heart: 50,
+                    like: 20,
+                    dislike: 1,
+                }
+            }
+
+        ]
+    }
+]
+
+const posts = [
+
+]
+
+
+module.exports = { posts, comments }
