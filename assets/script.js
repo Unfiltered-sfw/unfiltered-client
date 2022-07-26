a2c56ba1b4f37edc8df474d74237725a330854ac
agiskorska <skorskaga@gmail.com>
2022-07-26 17:26:25 +0100

    Send post requests for new posts and reactions

--- assets/script.js
+++ assets/script.js
@@ -10,38 +10,39 @@ const state = {
 
 // reaction handlers
 const sendReaction = (reaction, count, id, type) => { //either postID or commentID
-    let commOrPost;
+    let suffix =''
     if( type == 'post') {
+        suffix = 'posts'
         state.data.filter(post => {
             if( post.id == id) {
                 state.data[id].reaction[`${reaction}`] = count
             }            
         })
     } else if (type === 'comment') {
+        suffix = 'comments'
         state.comments.filter((commentArr, index) => {
             commentArr.comments.filter(comment => {
                 if( comment.id == id) {
                     state.comments[index].comments[id].reaction[`${reaction}`] = count
-                    console.log(state.comments[index].comments[id].reaction[`${reaction}`])
-
                 }
             })
         })
     }  
-    fetch(rootUrl,
-        {
-            headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-            },
-            method: 'POST',
-            body: JSON.stringify(state)
-        })
-        .then(res => console.log(res))
-        .catch(err => console.log(err))
+    fetch(rootUrl + suffix,
+    {
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(state.comments)
+    })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
 }
+
+
 const addReactionToPost = (e) => {
-    console.log('click')
     e.preventDefault();
     const span = e.currentTarget.querySelector('span')
     const reaction = span.dataset.reaction
@@ -52,6 +53,50 @@ const addReactionToPost = (e) => {
     span.textContent = newCount;
     sendReaction(reaction, newCount, id, type)
 }
+
+// Set new post handlers
+const createNewPost = (e) => {
+    e.preventDefault();
+    id = state.data.length
+    const newDataObject = {
+        id: id,
+        title: e.target.elements[0].value,
+        post: e.target.elements[1].value,
+        reaction: {
+            heart: 0,
+            like: 0,
+            dislike: 0,
+        }
+    }
+
+    const newCommentsObject = {
+        postId: id,
+        name: '',
+        comments: [
+
+        ]
+    }
+    const newData = {data: [newDataObject]}
+    const newComm = {comments: [newCommentsObject]}
+    state.data.push(newDataObject)
+    state.comments.push(newCommentsObject)
+    printContent(newData, newComm)
+
+    fetch(rootUrl + 'posts', {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(state.data)
+    })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+}
+
+form.addEventListener('submit', createNewPost);
+
 
 // Handlebars handlers
 
@@ -70,16 +115,8 @@ const assignCommentsToPosts = (data, comments) => {
     return arr
 }
 
-
-
-// Fetch data
-fetch(rootUrl+'posts')
-    .then(res => res.json())
-    .then(data => {
-        fetch(rootUrl+ "comments")
-            .then(res => res.json())
-            .then(comments => {
-                const postInfo = document.getElementById("post-template").innerHTML;
+const printContent = (data, comments) => {
+    const postInfo = document.getElementById("post-template").innerHTML;
                 const template = Handlebars.compile(postInfo);
                 const preparedObj = assignCommentsToPosts(data.data,comments.comments);
                 const postData = template({
@@ -89,16 +126,28 @@ fetch(rootUrl+'posts')
                 
                 const postReactionParent = document.querySelectorAll('.post-reactions')
 
-                const commentReactionParent = document.querySelectorAll('comment-reaction')
                 for (let block of postReactionParent ) {
                     const spans = block.querySelectorAll('button');
                     for (let span of spans) {
                         span.addEventListener('click', addReactionToPost);
                     }
                 }
+}
+
+// Fetch data
+fetch(rootUrl+'posts')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        fetch(rootUrl+ "comments")
+            .then(res => res.json())
+            .then(comments => {
                 state.data = data.data
                 state.comments = comments.comments
+                console.log(state.data)
+                printContent(data, comments)
             })
         })
         
+
 
